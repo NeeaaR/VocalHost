30da7bca6706ce9070775d2460b2e167394c9b91
=Hostie <dylan.levraud@ynov.com>
2020-03-26 14:45:29 +0100

    commit test

--- src/Pages/Test.js
+++ src/Pages/Test.js
@@ -1,30 +1,29 @@
 import React from 'react';
 
-
 class Test extends React.Component {
+	state = {
+		person: null
+	};
 
-    state = {
-        loading: true,
-        person: null
-    };
+	//Permet de récupérer l'API. On a besoin d'une requête asynchrone (node etc)
+	async componentDidMount() {
+		//On définit la route de l'api pour réupérer les users
+		const url = 'http://localhost:8000/api/v1/users';
+		const response = await fetch(url);
+		const data = await response.json();
+		console.log(data);
+		this.setState({ person: data });
+	}
 
-    //Permet de récupérer l'API. On a besoin d'une requête asynchrone (node etc)
-    async componentDidMount() {
-        //On définit la route de l'api pour réupérer les users
-        const url = "http://localhost:8000/api/v1/users"
-        const response = await fetch(url)
-        const data = await response.json()
-        console.log(data)
-        this.setState({ person: data.users[0] })
-    }
-
-    render() {
-        return (
-            <div>
-                <div> {this.state.person.email} </div>
-            </div>
-        )
-    }
+	render() {
+		return (
+			<div>
+				<div>
+					<p>{this.state.person.email}</p>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default Test;
