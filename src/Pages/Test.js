11e92a4714aef8afe1c53cfe1bf29880d1d05689
=Hostie <dylan.levraud@ynov.com>
2020-03-26 16:03:39 +0100

    commit dans ta mère

--- src/Pages/Test.js
+++ src/Pages/Test.js
@@ -1,26 +1,28 @@
 import React from 'react';
+import axios from 'axios';
 
 class Test extends React.Component {
 	state = {
-		person: null
+		person: []
 	};
 
 	//Permet de récupérer l'API. On a besoin d'une requête asynchrone (node etc)
 	async componentDidMount() {
 		//On définit la route de l'api pour réupérer les users
 		const url = 'http://localhost:8000/api/v1/users';
-		const response = await fetch(url);
-		const data = await response.json();
-		console.log(data);
-		this.setState({ person: data });
+		const self = this;
+
+		axios.get(url).then(function(res) {
+			console.log(res.data[0]);
+			self.setState({ person: res.data[0] });
+		});
 	}
 
 	render() {
 		return (
 			<div>
-				<div>
-					<p>{this.state.person.email}</p>
-				</div>
+				<p>{this.state.person.email}</p>
+				<p>{this.state.person.firstname}</p>
 			</div>
 		);
 	}
